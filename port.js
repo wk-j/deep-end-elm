6a59b562b84eb8cda001e1069a1897879ad9c1dc
wk-j <somnuk.wk@gmail.com>
2017-08-08 01:55:02 +0700

    Added action

--- port.js
+++ port.js
@@ -1,4 +1,3 @@
 const Elm = require("./output/elm.js");
-
 let container = document.getElementById("container");
 let deepEnd = Elm.Main.embed(container);
--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,89 @@
 module Main exposing (..)
-
 import Html exposing (Html, button, div, text)
+import Html exposing ( Html, button, div, text, span, header, footer, h1, ul, li, img, strong, p, iframe)
 import Html.Events exposing (onClick)
-import Template 
+import Html.Attributes exposing (classList, class, src, width, height)
+import Html.Events exposing (onClick)
 
-type alias Model = Int
-type Msg = Increase | Decrease
+type alias Video = { title : String, source : String } 
+type alias Model = { videos : List Video, currentVideo : String }
+type Msg = Play String
+
+-- ##############################################################
+-- ##############################################################
+
+headerN : Html msg
+headerN = 
+    header 
+        [ class "toolbar toolbar-header" ] 
+        [ div 
+            [ class "toolbar-actions" ] 
+            [ button 
+                [ class "btn btn-default pull-right"] 
+                [span [ class "icon icon-facebook"] []]
+            ]
+        ]
+        
+listItemN current video =
+    li 
+        [classList [("list-group-item", True), ("active", current == video.source)] , onClick (Play video.source)]
+        [
+            img [classList [("img-circle media-object pull-left",True)], src "images/large-round.png", width 32, height 32] [],
+            div [class "media-body"] 
+                [
+                    strong [] [text video.title],
+                    p [] [text "Jannine Weigel"]
+                ]
+        ]
+
+listGroupN model = 
+    ul  [class "list-group"]
+        (model.videos |> List.map (listItemN model.currentVideo))
+
+footerN : Html msg
+footerN = 
+    footer 
+        [class "toolbar toobar-footer"]
+        [ h1 [class "title"] [] ]
+
+contentN model = 
+    div 
+        [class "window-content"]
+        [
+            div [class "pane"] 
+                [(listGroupN model)],
+            div [class "pane"] 
+                [
+                    iframe [width 560, height 315, src model.currentVideo] []
+                ]
+        ]
+
+windowN model = 
+    div 
+        [ class "window" ]
+        [ 
+            headerN,
+            contentN model,
+            footerN 
+        ]
+
+-- ##############################################################
+-- ##############################################################
 
 model : Model
-model = 0
-
-update : Msg -> Model -> Model
-update  msg model = 
-    case msg of 
-        Increase -> model + 1
-        Decrease -> model - 1
+model = 
+    { currentVideo = "https://www.youtube.com/embed/NCXfKyfpBKI?ecver=1&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1", 
+      videos = [
+            { title = "Deep End", source = "https://www.youtube.com/embed/NCXfKyfpBKI?ecver=1&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1"} ,
+            { title = "Heart Stop", source = "https://www.youtube.com/embed/lBN9VDFDvOk?ecver=1&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1"} ,
+            { title = "Strangled Love", source = "https://www.youtube.com/embed/zeP7bqMySmE?ecver=1&autoplay=1&controls=0&showinfo=0&rel=0&modestbranding=1"} 
+      ]}
 
 view : Model -> Html Msg
-view model = 
-    Template.windowN
+view model = windowN model 
 
-    {-
-    div []
-        [ button [onClick Decrease] [text "-"]
-        , div [] [text (toString model)]
-        , button [onClick Increase] [text "+"]
-        ]
-    -}
+update : Msg -> Model -> Model 
+update (Play video) model = { model | currentVideo = video }
 
 main : Program Never Model Msg
 main = Html.beginnerProgram { model = model, view = view, update = update }
